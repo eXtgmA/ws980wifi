d1195c6a95e76d420a17c090aa50e01d35703147
Patrick Schmid <patrick.schmid@geoprevent.com>
2022-12-28 10:25:13 +0100

    cleanup debug code

--- custom_components/ws980wifi/sensor.py
+++ custom_components/ws980wifi/sensor.py
@@ -218,10 +218,7 @@ class WeatherData(Entity):
                     new_state = None
                 else:
                     new_state = float(int(new_state,16)) / sensor._decimalPlace
-                    _LOGGER.info("New state for %s: %s", sensor._name, new_state)
-                    if sensor._name == "outside temperature" and int(new_state) > 100:
-                      _LOGGER.info("Overload outside temperatur: %s", new_state)
-                      new_state = None
+                    _LOGGER.debug("New state for %s: %s", sensor._name, new_state)
             else:
                 _LOGGER.debug("Data is not 164 long, NONE")
             if new_state != sensor._state:
